2cb213b9e0a1b1ea55252a3c3cdeb7f08f18a8ee
Ralph <ralph@chikwamapay.com>
2024-10-11 16:21:49 +0200

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,49 @@
+﻿using IdentityModel.Client;
+using System.Text.Json;
+
+// Discover Endpoints 
+var client = new HttpClient();
+var disco = await client.GetDiscoveryDocumentAsync("https://localhost:5001");
+if (disco.IsError)
+{
+    Console.WriteLine(disco.Error);
+    Console.WriteLine(disco.Exception);
+    Console.ReadKey();
+    return 1;
+}
+
+// Request Token
+var tokenResponse = await client.RequestClientCredentialsTokenAsync(
+    new ClientCredentialsTokenRequest
+    {
+        Address = disco.TokenEndpoint,
+        ClientId = "client",
+        ClientSecret = "secret",
+        Scope = "api"
+    });
+if (tokenResponse.IsError)
+{
+    Console.WriteLine(tokenResponse.Error);
+    Console.WriteLine(tokenResponse.ErrorDescription);
+    Console.ReadKey();
+    return 1;
+}
+Console.WriteLine(tokenResponse.AccessToken);
+
+
+//Calling API 
+var apiClient = new HttpClient();
+apiClient.SetBearerToken(tokenResponse.AccessToken);
+
+var response = await apiClient.GetAsync("https://localhost:6001/identity");
+if (!response.IsSuccessStatusCode)
+{
+    Console.WriteLine($"{response.StatusCode}");
+    Console.ReadKey();
+    return 1;
+}
+
+var doc = JsonDocument.Parse(await response.Content.ReadAsStringAsync()).RootElement;
+Console.WriteLine(JsonSerializer.Serialize(doc, new JsonSerializerOptions { WriteIndented = true}));
+Console.ReadKey();
+return  0;
---
+++ IdentityServer/Config.cs
@@ -0,0 +1,32 @@
+﻿using Duende.IdentityServer.Models;
+
+namespace IdentityServer;
+
+public static class Config
+{
+    public static IEnumerable<IdentityResource> IdentityResources =>
+        new IdentityResource[]
+        { 
+            new IdentityResources.OpenId()
+        };
+
+    public static IEnumerable<ApiScope> ApiScopes =>
+        new ApiScope[]
+            {
+                new ApiScope(name : "api", displayName : "My API")
+            };
+
+    public static IEnumerable<Client> Clients =>
+        new Client[] 
+            {
+                new Client
+                {
+                    ClientId = "client",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets = { 
+                    new Secret("secret".Sha256())
+                    },
+                    AllowedScopes= { "api" }
+                }
+            };
+}
